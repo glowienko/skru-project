7aae9fd4405896673b221fc6fb70321bedeca3d4
Patryk <glowienko.patryk@gmail.com>
2017-05-29 23:58:17 +0200

    lombok setup in project

--- app/src/main/java/project/skru_dane_po_warszawsku/MainActivity.java
+++ app/src/main/java/project/skru_dane_po_warszawsku/MainActivity.java
@@ -1,7 +1,8 @@
 package project.skru_dane_po_warszawsku;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,14 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        Lambda kappa = (int a, int b) -> a + b;
     }
 
 }
 
-
-interface Lambda{
-    int addCap (int a, int b);
-}
